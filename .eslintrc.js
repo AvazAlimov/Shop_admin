0b7d2f2be7b1abf6fcce1be64b5d702ae42f5795
Faridun Klichov <faridunklichov@Fariduns-MacBook-Air.local>
2018-11-28 22:18:29 +0500

    Setup project: - installed Vue - installed Eslint - installed Pug

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+      sourceType: 'module'
+    },
+    env: {
+      browser: true,
+    },
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    extends: [
+      'standard',
+      'plugin:vue/essential'],
+    // required to lint *.vue files
+    plugins: [
+      "vue"
+    ],
+    // add your custom rules here
+    rules: {
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    }
+}
