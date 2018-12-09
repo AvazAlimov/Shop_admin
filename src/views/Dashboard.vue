ddf14fdd5c291f3aa2eecb168b3a5bff1d63564b
AvazAlimov <avazalimov95@mail.ru>
2018-12-09 22:20:29 +0500

    Dashboard design created

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,43 @@
 <template lang="pug">
-  v-app
+v-app
+    v-navigation-drawer(
+        v-model="menu" app fixed
+        :clipped="$vuetify.breakpoint.lgAndUp"
+    )
+        v-list
+            v-list-tile(to="/")
+                v-list-tile-action
+                    v-icon home
+                v-list-tile-title Главная
+            v-list-tile(to="/brands")
+                v-list-tile-action
+                    v-icon star
+                v-list-tile-title Бренды
+            v-list-tile(to="/collections")
+                v-list-tile-action
+                    v-icon collections
+                v-list-tile-title Коллекции
+            v-list-tile(to="/products")
+                v-list-tile-action
+                    v-icon redeem
+                v-list-tile-title Товары
+            v-list-tile(to="/settings")
+                v-list-tile-action
+                    v-icon settings
+                v-list-tile-title Настройки сайта
+            v-divider
+            v-list-tile(@click="logout")
+                v-list-tile-action
+                    v-icon forward
+                v-list-tile-title Выйти
+
+    v-toolbar(color="primary" app dark fixed :clipped-left="$vuetify.breakpoint.lgAndUp")
+                v-toolbar-side-icon(@click.stop="menu=!menu")
+                v-toolbar-title Панель администратора
+
+    v-content
+            v-container(grid-list-md fluid)
+                router-view
 </template>
 
 <script>
@@ -9,6 +47,11 @@ import { mapGetters, mapMutations } from 'vuex'
 
 export default {
     name: 'Dashboard',
+    data () {
+        return {
+            menu: false
+        }
+    },
     computed: {
         ...mapGetters({
             token: 'auth/getToken'
@@ -17,7 +60,11 @@ export default {
     methods: {
         ...mapMutations({
             _logout: 'auth/logout'
-        })
+        }),
+        logout () {
+            this._logout()
+            this.$router.push('/login')
+        }
     },
     created () {
         if (!this.token) {
@@ -26,7 +73,7 @@ export default {
             AXIOS.defaults.headers.common['Authorization'] = 'Bearer ' + this.token
             AXIOS.interceptors.response.use(undefined, error => {
                 if (error.response.status) {
-                    this._logout()
+                    this.logout()
                 }
             })
             Auth.details()
